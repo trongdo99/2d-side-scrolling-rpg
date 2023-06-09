37c4b04b4f8abfecd8e44f4cd026aadf47aaa11b
Trong Do <trong.do-duc@ubisoft.com>
2023-06-09 18:19:23 +0700

    WIP

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -3,44 +3,101 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class CharacterController2D : MonoBehaviour
 {
-    public struct RaycastOrigin
-    {
-        public Vector2 topLeft;
-        public Vector2 bottomLeft;
-        public Vector2 topRight;
-        public Vector2 bottomRight;
-    }
-
     [SerializeField] private LayerMask _collisionMask;
     [SerializeField] private float _skinWidth;
     [SerializeField] private int _horizontalRayCount;
     [SerializeField] private int _verticalRayCount;
 
+    private Rigidbody2D _rb;
     private RaycastOrigin _raycastOrigins;
+    private CollisionInfo _collisionInfo;
     private BoxCollider2D _boxCollider;
     private float _horizontalRaySpacing;
     private float _verticalRaySpacing;
+    private float _lastXVelocity;
+
+    public CollisionInfo CollisionInfo => _collisionInfo;
 
     private void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
         _boxCollider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
     }
 
-    private void Update()
+    public void Move(Vector2 velocity)
     {
         UpdateRaycastOrigins();
-        CalculateRaySpacing();
-        for (int i = 0; i < _horizontalRayCount; i++)
+        _collisionInfo.Reset();
+
+        if (velocity.x != 0)
         {
-            Debug.DrawRay(new Vector2(_raycastOrigins.bottomLeft.x + _horizontalRaySpacing * i, _raycastOrigins.bottomLeft.y), Vector2.down * 0.2f, Color.red);
+            HorizontalCollision(velocity);
         }
+
+        if (velocity.y != 0)
+        {
+            VerticalCollision(velocity);
+        }
+
+        _rb.velocity = velocity;
+        _lastXVelocity = _rb.velocity.x;
+    }
+
+    private void VerticalCollision(Vector2 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + _skinWidth;
 
         for (int i = 0; i < _verticalRayCount; i++)
         {
-            Debug.DrawRay(new Vector2(_raycastOrigins.topLeft.x, _raycastOrigins.topLeft.y - _verticalRaySpacing * i), Vector2.left * 0.2f, Color.blue);
+            Vector2 raycastOrigin = directionY == -1 ? _raycastOrigins.bottomLeft : _raycastOrigins.topLeft;
+            raycastOrigin += Vector2.right * (_verticalRaySpacing * i + (Mathf.Abs(velocity.x - _lastXVelocity)));
+
+            RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.up * directionY, rayLength, _collisionMask);
+            if (hit)
+            {
+                rayLength = hit.distance;
+
+                _collisionInfo.above = directionY == 1;
+                _collisionInfo.below = directionY == -1;
+
+                Debug.DrawRay(raycastOrigin, Vector2.up * directionY * rayLength, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(raycastOrigin, Vector2.up * directionY * rayLength, Color.green);
+            }
+        }
+    }
+
+    private void HorizontalCollision(Vector2 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + _skinWidth;
+
+        for (int i = 0; i < _horizontalRayCount; i++)
+        {
+            Vector2 raycastOrigin = directionX == -1 ? _raycastOrigins.bottomLeft : _raycastOrigins.bottomRight;
+            raycastOrigin += Vector2.up * (_horizontalRaySpacing * i);
+
+            RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.right * directionX, rayLength, _collisionMask);
+            if (hit)
+            {
+                rayLength = hit.distance;
+
+                _collisionInfo.right = directionX == 1;
+                _collisionInfo.left = directionX == -1;
+
+                Debug.DrawRay(raycastOrigin, Vector2.right * directionX * rayLength, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(raycastOrigin, Vector2.right * directionX * rayLength, Color.green);
+            }
         }
     }
 
@@ -52,8 +109,8 @@ public class CharacterController2D : MonoBehaviour
         _horizontalRayCount = Mathf.Clamp(_horizontalRayCount, 2, int.MaxValue);
         _verticalRayCount = Mathf.Clamp(_verticalRayCount, 2, int.MaxValue);
 
-        _horizontalRaySpacing = bound.size.x / (_horizontalRayCount - 1);
-        _verticalRaySpacing = bound.size.y / (_verticalRayCount - 1);
+        _horizontalRaySpacing = bound.size.y / (_horizontalRayCount - 1);
+        _verticalRaySpacing = bound.size.x / (_verticalRayCount - 1);
     }
 
     private void UpdateRaycastOrigins()
@@ -67,3 +124,23 @@ public class CharacterController2D : MonoBehaviour
         _raycastOrigins.bottomRight = new Vector2(bound.max.x, bound.min.y);
     }
 }
+
+public struct RaycastOrigin
+{
+    public Vector2 topLeft;
+    public Vector2 bottomLeft;
+    public Vector2 topRight;
+    public Vector2 bottomRight;
+}
+
+public struct CollisionInfo
+{
+    public bool above, below;
+    public bool left, right;
+
+    public void Reset()
+    {
+        above = below = left = right = false;
+    }
+}
+
