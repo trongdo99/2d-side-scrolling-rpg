2b6c55ae221bc5af83fed509e336d70a86a7145f
Trong Do <trong.do-duc@ubisoft.com>
2023-06-09 18:19:54 +0700

    WIP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private Transform[] _groundRaycasts;
 
+    private CharacterController2D _controller;
     private Rigidbody2D _rb;
     private Vector2 _inputVector;
     private float _jumpForce;
@@ -23,6 +24,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _controller = GetComponent<CharacterController2D>();
 
         float gravity = -2 * _maxJumpHeight / Mathf.Pow(_timeToJumpApex, 2);
         _gravityScale = gravity / Physics2D.gravity.y;
@@ -59,16 +61,22 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        _isGrounded = false;
-        foreach (var transform in _groundRaycasts)
+        if (_controller.CollisionInfo.below)
         {
-            var hit = Physics2D.Raycast(transform.position, Vector2.down, 0.2f, _groundLayer);
-            if (hit)
-            {
-                _isGrounded = true;
-                _rb.gravityScale = _gravityScale;
-            }
+            _rb.gravityScale = _gravityScale;
         }
+
+        //_isGrounded = false;
+
+        //foreach (var transform in _groundRaycasts)
+        //{
+        //    var hit = Physics2D.Raycast(transform.position, Vector2.down, 0.2f, _groundLayer);
+        //    if (hit)
+        //    {
+        //        _isGrounded = true;
+        //        _rb.gravityScale = _gravityScale;
+        //    }
+        //}
 
         if (!_isApexReached && _lastYPosition > _rb.position.y)
         {
@@ -83,12 +91,14 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //_controller.Move(_inputVector * _moveSpeed);
+        _rb.velocity *= Vector2.right * _inputVector.x * _moveSpeed;
         _rb.velocity = new Vector2(_inputVector.x * _moveSpeed, _rb.velocity.y);
     }
 
     private void GameInputManager_OnJumpAction()
     {
-        if (_isGrounded)
+        if (_controller.CollisionInfo.below)
         {
             _rb.AddForce(new Vector2(0f, _jumpForce), ForceMode2D.Impulse);
             _lastYPosition = Mathf.NegativeInfinity;
